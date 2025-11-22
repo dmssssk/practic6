d1f5cd734d790e9feae709436c04711624810944
dimassssek <dimaivashin2908@gmail.com>
2025-11-22 14:27:51 +0500

    v0.1

---
+++ Parser.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace Practic6;
+
+public class Parser
+{
+    private readonly SettingsPars _settings;
+    private readonly StreamReader _reader;
+    
+
+    public Parser()
+    {
+        _settings = Input();
+        _reader = new StreamReader(_settings.FullPath);
+        
+        string[] tablesFile = MyRegEx.TablesFile(_reader.ReadLine()!, _settings.Extension);
+        foreach (string tableNeed in _settings.Rows.Split(' '))
+        {
+            for (int i = 0; i < tablesFile.Length; i++)
+            {
+                if (tableNeed == tablesFile[i])
+                {
+                    _settings.TablesNeedNums.Add(i);
+                }
+            }
+        }
+    }
+    
+    
+    private SettingsPars Input()
+    {
+        string folder = "", file = "", pagination = "", rows = "";
+        bool flag = false;
+
+        while (!flag)
+        {
+            folder = Console.ReadLine()!;
+            if (Directory.Exists(folder))
+            {
+                flag = true;
+            }
+        }
+
+        flag = false;
+        while (!flag)
+        {
+            file = Console.ReadLine()!;
+            if (File.Exists($"{folder}/{file}"))
+            {
+                flag = true;
+            }
+        }
+        
+        
+        flag = false;
+        while (!flag)
+        {
+            pagination = Console.ReadLine()!;
+            string parsPag = MyRegEx.Pagination(pagination);
+            if (parsPag != "")
+            {
+                string[] sPag = parsPag.Split('-');
+                if (int.Parse(sPag[1]) > int.Parse(sPag[0]))
+                {
+                    flag = true;
+                }
+            }
+        }
+
+        rows = Console.ReadLine()!;
+        return new SettingsPars(folder, file, pagination, rows);
+    }
+
+    // private bool CheckValid()
+    // {
+    //     string[] tablesFile = MyRegEx.TablesFile(_reader.ReadLine()!, _settings.Extension);
+    //     return false;
+    // }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practic6;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string s = "GYDFds\tfsdfs";
+        
+        // Console.WriteLine(MyRegEx.TablesFile(s));
+
+        foreach (string ss in MyRegEx.TablesFile(s, ".tsv"))
+        {
+            Console.WriteLine(ss);
+        }
+        
+        StreamReader gg = new StreamReader("C:\\Users\\dimai\\RiderProjects\\Practic6\\Practic6\\testFiles\\contractors.csv");
+        string sss;
+        for(int i = 0; i < 1000; i++)
+        {
+            sss = gg.ReadLine();
+            if (sss != null)
+            {
+                Console.WriteLine(sss);
+            }
+        }
+
+
+
+    }
+}
