8773bb9ba066cc1ba751ec3f1b7d81bc35d83d89
dimassssek <dimaivashin2908@gmail.com>
2025-11-22 19:38:02 +0500

    v1.1
    небольшое форматирование кода

---
+++ RegEx.cs
@@ -0,0 +1,16 @@
+﻿namespace Practic6;
+using System.Text.RegularExpressions;
+
+
+public static class MyRegEx
+{
+    public static string Pagination(string pag)
+    {
+        return new Regex(@"[1-9]\d*-[1-9]\d*").Match(pag).ToString();
+    }
+    
+    public static string[] TablesFile(string firstString, char sep)
+    {
+        return new Regex(@$"(\w+)|(\w+{sep}\w+)+").Matches(firstString).Select(m => m.Value).ToArray();
+    }
+}
